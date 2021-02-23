baa19af8c08c7e4954dc2e29862e5599014c191a
Marco Wang <m.aesophor@gmail.com>
2021-02-24 04:29:53 +0800

    kmain: initialize .bss to 0

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -1,12 +1,18 @@
 // Copyright (c) 2021 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
 
-extern "C" void kmain(char* bss_start, char* bss_end) {
-  // Initialize .bss section to 0
-  char *ptr = bss_start;
-  while (ptr <= bss_end) {
-    *ptr = 0x00;
-    ++ptr;
-  }
+namespace {
 
+// Initialize .bss section to 0
+void init_bss(char* begin, char* end) {
+  while (begin <= end) {
+    *begin++ = 0x00;
+  }
+}
+
+}  // namespace
+
+
+extern "C" void kmain(char* bss_start, char* bss_end) {
+  init_bss(bss_start, bss_end);
   while (1);
 }
