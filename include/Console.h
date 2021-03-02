a4c0039d9c6c0f84e9593ffd1e756b42f7364d25
Marco Wang <m.aesophor@gmail.com>
2021-03-03 04:25:15 +0800

    refactor Q_Q

--- include/Console.h
+++ include/Console.h
@@ -5,20 +5,14 @@
 #include <Types.h>
 #include <MiniUART.h>
 
-namespace {
-
-valkyrie::kernel::MiniUART* mini_uart;
-
-}  // namespace
-
-
 namespace valkyrie::kernel {
 
 void console_init(MiniUART* mini_uart);
 
-void gets(char* s);
-void puts(const char* s);
+char getchar();
 void putchar(const char c);
+void gets(char* s);
+void puts(const char* s, bool newline = true);
 void print_hex(const uint32_t value);
 
 }  // namespace valkyrie::kernel
