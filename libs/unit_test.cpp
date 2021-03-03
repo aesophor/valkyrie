201c13e00b2afcfc792c46b3d350871a3b08ee57
p870613 <p870613@yahoo.com.tw>
2021-03-03 16:30:57 +0800

    strncpy finish

--- libs/unit_test.cpp
+++ libs/unit_test.cpp
@@ -2,15 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
-char* test_strcpy(char* dest, const char* src){
+char* test_strncpy(char* dest, const char* src, size_t n){
     if(dest == NULL)
         return NULL;
 
     char *ret = dest;
 
-    while(*src != '\0'){
+    while(*src != '\0' && n --){
         *dest++ = *src++;
     }
+
     *dest = '\0';
 
     return ret;
@@ -23,9 +24,9 @@ int main(){
     int n;
     while(true){
         //scanf("%s %s %d", s1, s2, &n);
-        scanf("%s", s1);
-        char* rec =  test_strcpy(s2, s1);
-        int a =  strcmp(s1, rec);
+        scanf("%s %d", s1, &n);
+        char* rec =  test_strncpy(s2, s1, n);
+        int a =  strncmp(s1, rec, n);
         if(a){
             std::cout << s1 << " " << s2 << " " << "false" << std::endl;
         }else{
