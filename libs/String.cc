4e39a1d8a4e88936b393c6fe63b4be66ad42f220
Marco Wang <m.aesophor@gmail.com>
2021-03-03 08:02:03 +0800

    libs: String: clean up

--- libs/String.cc
+++ libs/String.cc
@@ -43,76 +43,65 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-   if(n == 0)
-       return 0;
-
-    while(n--){
-        if(*s1 != *s2)
-            return *s1 - *s2;
-        else{
-            s1 ++;
-            s2 ++;
-        }
+  while ((*s1 || *s2) && n--) {
+    if (*s1 == *s2) {
+      ++s1;
+      ++s2;
+    } else {
+      return *s1 - *s2;
     }
-    return 0;
+  }
+  return 0;
 }
 
 char* strcpy(char* dest, const char* src) {
-    if(dest == nullptr)
-        return nullptr;
-
-    char *ret = dest;
-
-    while(*src != '\0'){
-        *dest++ = *src++;
-    }
-    *dest = '\0';
-
-    return ret;
+  char* ret = dest;
+  while (*src) {
+    *dest++ = *src++;
+  }
+  *dest = '\0';
+  return ret;
 }
 
 char* strncpy(char* dest, const char* src, size_t n) {
-    if(dest == nullptr)
-        return nullptr;
-
-    char *ret = dest;
-
-    while(*src != '\0' && n --){
-        *dest++ = *src++;
-    }
-
-    *dest = '\0';
-
-    return ret;
-
+  char* ret = dest;
+  while (*src && n--) {
+    *dest++ = *src++;
+  }
+  *dest = '\0';
+  return ret;
 }
 
 char* strcat(char* dest, const char* src) {
-    char *cur = dest + strlen(dest);
-
-    while(*src != '\0'){
-        *cur++ = *src++;
-    }
-
-    *cur = '\0';
-
-    return dest;
+  char* cur = dest + strlen(dest);
+  while (*src) {
+    *cur++ = *src++;
+  }
+  *cur = '\0';
+  return dest;
 }
 
-int compare(const char* x, const char* y){
-    while(*x && *y){
-        if(*x != *y)
-            return 0;
-        x++;
-        y++;
-    }
-    return (*y == '\0');
-}
 char* strstr(const char* haystack, const char* needle){
-    while(*haystack != '\0'){
-        if(*haystack == *needle && compare(haystack, needle))
-            return ((char*)haystack);
-        haystack ++;
-    }
+  int haystack_size = strlen(haystack);
+  int needle_size = strlen(needle);
+
+  if (!needle_size) {
     return nullptr;
+  }
+
+  for (int i = 0; i <= haystack_size - needle_size; i++) {
+    bool matched = true;
+
+    for (int j = 0; j < needle_size; j++) {
+      if (haystack[i + j] != needle[j]) {
+        matched = false;
+        break;
+      }
+    }
+
+    if (matched) {
+      return const_cast<char*>(haystack + i);
+    }
+  }
+  return nullptr;
 }
