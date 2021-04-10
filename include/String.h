1a689ebf826f00d4f45ed2da31d0335d3ef28f25
Marco Wang <m.aesophor@gmail.com>
2021-04-11 05:04:56 +0800

    add String

--- include/String.h
+++ include/String.h
@@ -2,7 +2,9 @@
 #ifndef VALKYRIE_STRING_H_
 #define VALKYRIE_STRING_H_
 
+#include <kernel/Kernel.h>
 #include <UniquePtr.h>
+#include <libs/CString.h>
 
 namespace valkyrie::kernel {
 
@@ -12,29 +14,52 @@ class String {
   String() : _s() {}
 
   // Constructor
-  explicit
-  String(char* s) : _s(s) {}
+  String(const char* s)
+      : _s(make_unique<char[]>(strlen(s) + 1)) {
+    strcpy(_s.get(), s);
+  }
 
   // Destructor
   ~String() = default;
 
   // Copy constructor
-  String(const String&) {}
+  String(const String& other) {
+    *this = other;
+  }
 
   // Copy assignment operator
   String& operator =(const String& other) {
+    _s = make_unique<char[]>(other.size() + 1);
+    strcpy(_s.get(), other._s.get());
     return *this;
   }
   
   // Move constructor
-  String(String&& other) noexcept {
-
+  String(String&& other) {
+    *this = move(other);
   }
  
   // Move assignment operator
   String& operator =(String&& other) noexcept {
+    _s = move(other._s);
     return *this;
   }
+
+
+  char& operator [](size_t i) { return _s[i]; }
+
+  String operator +(const String& other) const {
+    String ret;
+    ret._s = make_unique<char[]>(size() + other.size() + 1);
+    strcpy(ret._s.get(), _s.get());
+    strcat(ret._s.get(), other._s.get());
+    return ret;
+  }
+
+
+  size_t size() const { return (_s) ? strlen(_s.get()) : 0; }
+
+  const char* c_str() const { return _s.get(); }
 
  private:
   UniquePtr<char[]> _s;
