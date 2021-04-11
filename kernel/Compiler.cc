e548459cd75cc4b03f53e4a5d986fb8c9cabda34
Marco Wang <m.aesophor@gmail.com>
2021-04-11 08:12:04 +0800

    kernel: Compiler: provide __cxa_atexit and __dso_handle

--- kernel/Compiler.cc
+++ kernel/Compiler.cc
@@ -8,3 +8,11 @@ using valkyrie::kernel::Kernel;
 extern "C" void __cxa_pure_virtual(void) {
   Kernel::panic("__cxa_pure_virtual invoked\n");
 }
+
+extern "C" void __cxa_atexit(void) {
+
+}
+
+extern "C" void __dso_handle(void) {
+  Kernel::panic("__dso_handle invoked\n");
+}
