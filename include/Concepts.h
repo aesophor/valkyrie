3e9530ec87aee5372866d7cbc94237f28fee6cdc
Marco Wang <m.aesophor@gmail.com>
2021-04-14 23:13:28 +0800

    Concepts: add is_callable<T> concept

---
+++ include/Concepts.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
+#ifndef VALKYRIE_CONCEPTS_H_
+#define VALKYRIE_CONCEPTS_H_
+
+namespace valkyrie::kernel {
+
+template <typename T>
+concept is_callable = requires(T t) { t(); };
+
+}  // namespace valkyrie::kernel
+
+#endif  // VALKYRIE_CONCEPTS_H_
