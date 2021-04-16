371e3f29b4b90cbe551203e14725c32e2a65c0ae
Marco Wang <m.aesophor@gmail.com>
2021-04-16 11:57:10 +0800

    move boot.S to boot/

--- kernel/main.cc
+++ kernel/main.cc
@@ -5,12 +5,12 @@ using ctor_func_t = void (*)();
 extern ctor_func_t start_ctors;
 extern ctor_func_t end_ctors;
 
-// Valkyrie Kernel C++ entry point  ヽ(○´∀`○)ﾉ
 extern "C" [[noreturn]] void kmain(void) {
   // Invoke all static global constructors.
   for (ctor_func_t* ctor = &start_ctors; ctor != &end_ctors; ctor++) {
     (*ctor)();
   }
 
+  // Valkyrie Kernel C++ entry point  ヽ(○´∀`○)ﾉ
   valkyrie::kernel::Kernel::get_instance().run();
 }
