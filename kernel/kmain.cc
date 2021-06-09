2250bc5e71b7c619f9017679eb7e9cdf65019e65
Marco Wang <m.aesophor@gmail.com>
2021-06-09 12:56:27 +0800

    clear .bss in `kmain()`

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -1,11 +1,20 @@
 // Copyright (c) 2021 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
 #include <kernel/Kernel.h>
 
+#include <libs/CString.h>
+
+extern uint8_t* _bss_start;
+extern uint8_t* _bss_end;
+
 using ctor_func_t = void (*)();
 extern ctor_func_t start_ctors;
 extern ctor_func_t end_ctors;
 
+
 extern "C" [[noreturn]] void kmain(void) {
+  // Initialize .bss
+  memset(_bss_start, 0, _bss_end - _bss_start);
+
   // Invoke all static global constructors.
   for (ctor_func_t* ctor = &start_ctors; ctor != &end_ctors; ctor++) {
     (*ctor)();
