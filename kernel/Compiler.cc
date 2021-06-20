b213bd82f8de8ddd945dc9ae297cc732a4ae2902
Marco Wang <m.aesophor@gmail.com>
2021-06-20 23:06:00 +0800

    kernel: Compiler: declare __dso_handle as a void*

--- kernel/Compiler.cc
+++ kernel/Compiler.cc
@@ -1,16 +1,20 @@
 // Copyright (c) 2021 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
+//
+// Compiler.cc - Some routines required by the linker.
+// 
+// References:
+// [1] https://itanium-cxx-abi.github.io/cxx-abi/abi.html#dso-dtor-runtime-api
+
 #include <kernel/Kernel.h>
 
 using valkyrie::kernel::Kernel;
 
 extern "C" void __cxa_pure_virtual(void) {
-  Kernel::panic("__cxa_pure_virtual invoked (possibly due to use-after-free)\n");
+  Kernel::panic("__cxa_pure_virtual invoked (likely caused by use-after-free)\n");
 }
 
-extern "C" void __cxa_atexit(void) {
+extern "C" void __cxa_atexit(void) {}
 
-}
-
-extern "C" void __dso_handle(void) {
-  Kernel::panic("__dso_handle invoked\n");
-}
+// __dso_handle is a "guard" that is used to
+// identify dynamic shared objects during global destruction.
+void* __dso_handle = nullptr;
