cc61ce6ef076ec3019bc3831dce5c62c6fb59d6c
Marco Wang <m.aesophor@gmail.com>
2021-06-22 01:05:00 +0800

    scripts: send.sh: surround variables with double quotes to prevent globbing and word splitting

--- scripts/send.sh
+++ scripts/send.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 tty=/dev/ttyUSB0
-size=$(stat --printf="%s" $1)
-echo $size | sudo tee $tty
-sudo tee $tty < $1   
+size=$(stat --printf="%s" "$1")
+echo "$size" | sudo tee "$tty"
+sudo tee "$tty" < "$1"
