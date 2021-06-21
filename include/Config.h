e95f19d06f30bd539343e2660a2ca8093851b02b
Marco Wang <m.aesophor@gmail.com>
2021-06-22 00:52:00 +0800

    add Config.h

---
+++ include/Config.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2021 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
+#ifndef VALKYRIE_CONFIG_H_
+#define VALKYRIE_CONFIG_H_
+
+#define DEBUG
+
+#endif  // VALKYRIE_CONFIG_H_
