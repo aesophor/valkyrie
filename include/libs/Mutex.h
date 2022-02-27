593c1672faf6a9906ede31ed5343460c0fbd4119
Marco Wang <m.aesophor@gmail.com>
2022-02-27 18:08:51 +0800

    libs/Mutex: clean up RecursiveMutex
    Signed-off-by: Marco Wang <m.aesophor@gmail.com>

--- include/libs/Mutex.h
+++ include/libs/Mutex.h
@@ -9,58 +9,29 @@
 
 namespace valkyrie::kernel {
 
-class Mutex {
- public:
-  Mutex() : _is_locked() {}
-  ~Mutex() = default;
-
-  // Locks the mutex, blocks if the mutex is not available
-  [[gnu::always_inline]]
-  inline void lock() {
-    ExceptionManager::disableIRQs();
-    _is_locked = true;
-  }
-
-  // Tries to lock the mutex,
-  // returns if the mutex is not available.
-  [[gnu::always_inline]]
-  inline void try_lock() {
-
-  }
-
-  // Unlocks the mutex.
-  [[gnu::always_inline]]
-  inline void unlock() {
-    _is_locked = false;
-    ExceptionManager::enableIRQs();
-  }
-
- private:
-  volatile bool _is_locked;
-};
-
-
 class RecursiveMutex {
  public:
   RecursiveMutex() : _is_locked(), _depth() {}
   ~RecursiveMutex() = default;
 
   // Locks the mutex, blocks if the mutex is not available
-  [[gnu::always_inline]]
-  inline void lock() {
-    ExceptionManager::disableIRQs();
-    _is_locked = true;
-    _depth++;
+  void lock() {
+    if (ExceptionManager::is_activated()) [[likely]] {
+      ExceptionManager::disableIRQs();
+      _is_locked = true;
+      _depth++;
+    }
   }
 
   // Unlocks the mutex.
-  [[gnu::always_inline]]
-  inline void unlock() {
-    _depth--;
-    if (_depth == 0) {
-      _is_locked = false;
-      ExceptionManager::enableIRQs();
+  void unlock() {
+    if (ExceptionManager::is_activated()) [[likely]] {
+      if (!--_depth) {
+        _is_locked = false;
+        ExceptionManager::enableIRQs();
+      }
     }
+
     // XXX: This must not happen...
     if (_depth < 0) {
       while (1);
