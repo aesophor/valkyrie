a6e07b1e67acfb80157fa9ef723604391ad4d1aa
Marco Wang <m.aesophor@gmail.com>
2023-03-04 02:05:22 +0800

    Correctly initialize .bss to zero

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -3,8 +3,8 @@
 
 #include <CString.h>
 
-extern uint8_t *_bss_start;
-extern uint8_t *_bss_end;
+extern char _bss_start[0];
+extern char _bss_end[0];
 
 using ctor_func_t = void (*)();
 extern ctor_func_t start_ctors;
@@ -13,14 +13,11 @@ extern ctor_func_t end_ctors;
 using namespace valkyrie::kernel;
 
 extern "C" [[noreturn]] void kmain(void) {
-  // Initialize .bss
   memset(_bss_start, 0, _bss_end - _bss_start);
 
-  // Invoke all static global constructors.
   for (ctor_func_t *ctor = &start_ctors; ctor != &end_ctors; ctor++) {
     (*ctor)();
   }
 
-  // C++ entry point.
   Kernel::the().run();
 }
